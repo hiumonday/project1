904f5b5bff7a76056111c5edaa0b693fc8f6e8c1
hiumonday <hieunt04lm@gmail.com>
2024-10-02 19:31:33 +0700

    third commit

--- src/Equation2Degree.java
+++ src/Equation2Degree.java
@@ -1,3 +1,21 @@
-public class Equation2Degree {
+import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
+public class Equation2Degree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        double delta = b*b - 4*a*c;
+        if(delta < 0 ) System.out.println("NO SOLUTION");
+        else if (delta == 0) System.out.printf("%.2f", (double)(-b/(2*a)));
+        else {
+            double x1 = (-b + sqrt(delta))/(2*a);
+            double x2 = (-b - sqrt(delta))/(2*a);
+
+            System.out.printf("%.2f %.2f",x2,x1);
+        }
+    }
 }
--- src/TextReplacement.java
+++ src/TextReplacement.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TextReplacement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String p1 = scanner.nextLine();
+        String p2 = scanner.nextLine();
+        String p3 = scanner.nextLine();
+        String[] texts = p3.trim().split(" ");
+        for (int i = 0; i < texts.length; i++) {
+            if(texts[i].equals(p1)) texts[i] = p2;
+        }
+
+        p3 = String.join(" ",texts);
+        System.out.println(p3);
+
+    }
 }
